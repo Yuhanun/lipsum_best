718dce2c5e6a23b523a39d012b75ac0912b0e4b7
Yuhanun <y.citgez@student.utwente.nl>
2019-11-22 14:34:54 +0100

    Changed to C++ + update gitignore

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "cpprest/json.h"
+#include "cpprest/http_listener.h"
+#include "cpprest/uri.h"
+#include "cpprest/asyncrt_utils.h"
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+}
